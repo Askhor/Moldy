36376df116cda7ee5ed6003be0cb545c6a622621
guenthner <guenthner.jonathan@gmail.com>
2025-03-17 21:15:26 +0100

    Fixed bugs with new default feature

--- makefile
+++ makefile
@@ -10,3 +10,8 @@ upload:
 	vinc
 	make build
 	.venv/bin/python -m twine upload --repository pypi dist/* $(flags)
+
+reload:
+	make upload
+	pipx upgrade Moldy
+	pipx upgrade Moldy
--- moldy/molding.py
+++ moldy/molding.py
@@ -6,6 +6,7 @@ from moldy.logging import log, Color
 mold_include_re = re.compile("<!--\\s*Mold\\s*:\\s*([a-zA-Z0-9.\\\\/ _]+)\\s*-*-->")
 mold_comment_re = re.compile("//.*\n")
 
+
 def process_section(section: str):
     values = section.split("$$")
 
@@ -37,14 +38,17 @@ def process_moldings(moldings: Path):
 
 
 def parse_moldable(file: Path):
+    """
+    Converts the string contained in the file to an array of strings and things that are moldables (represented by a tuple containing a string)
+    """
     string = file.read_text()
     output = []
     index = 0
 
     for match in mold_include_re.finditer(string):
         span = match.span()
-        output.append(string[index:span[0]])
-        output.append((match.group(1),))
+        output.append(string[index:span[0]]) # read all text until the match
+        output.append((match.group(1),)) # append tuple containing key
         index = span[1]
 
     if index < len(string):
@@ -76,21 +80,25 @@ def apply_molding(template: list, molding: map, destination: str):
     dest_file.write_text(output)
 
 
+def add_defaults_to_molding(molding, default_molding):
+    for key, default_value in default_molding:
+        if key not in molding:
+            log("Placing default value for ", Color(5), key, Color(None), " as it is missing for ", Color(5),
+                molding["name"],
+                Color(None))
+            molding[key] = default_value
+
+
 def mold(file: Path, moldings: Path, destination: str):
     template = parse_moldable(file)
-    parsed_moldings = { molding["name"]: molding for molding in process_moldings(moldings)}
+    parsed_moldings = {molding["name"]: molding for molding in process_moldings(moldings)}
 
     if "default" in parsed_moldings:
-        default_molding = {}
-    else:
         default_molding = parsed_moldings["default"]
         del parsed_moldings["default"]
 
-    for name, molding in parsed_moldings:
-        for key, default_value in default_molding:
-            if key not in molding:
-                log("Placing default value for ", Color(5), key, Color(None), " as it is missing for ", Color(5), name, Color(None))
-                molding[key] = default_value
+        for _, molding in parsed_moldings.items():
+            add_defaults_to_molding(molding, default_molding)
 
-    for _, molding in parsed_moldings:
+    for _, molding in parsed_moldings.items():
         apply_molding(template, molding, destination)
